77bb065b5e506f9a33f2fe500bef9d54262edf18
Gabriel Ferrer <ferrer@hendrix.edu>
2019-01-28 15:18:49 -0600

    Initial commit

---
+++ app/src/main/java/edu/hendrix/ferrer/simpledemo/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.hendrix.ferrer.simpledemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button reverser;
+    private TextView userEntry;
+
+    public static String reversed(String s) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            result = s.charAt(i) + result;
+        }
+        return result;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        reverser = findViewById(R.id.doSomething);
+        userEntry = findViewById(R.id.userEntry);
+
+        reverser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userEntry.setText(reversed(userEntry.getText().toString()));
+            }
+        });
+    }
+}
---
+++ app/src/test/java/edu/hendrix/ferrer/simpledemo/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.hendrix.ferrer.simpledemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void reverse_isCorrect() {
+        assertEquals("esrever", MainActivity.reversed("reverse"));
+    }
+}
